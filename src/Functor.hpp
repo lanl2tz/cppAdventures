0cb5e2fba3d602f8dfd43873746c5e9e2ad5082c
lanl2tz <lanl2tz@hotmail.com>
2024-10-20 08:22:07 +0800

    functor

---
+++ src/Functor.hpp
@@ -0,0 +1,57 @@
+/**
+ * Functor type in C++.
+ */
+
+#include <vector>
+#include <functional>
+#include <stdio.h>
+#include <iostream>
+
+template <typename T1>
+class Functor
+{
+private:
+    std::vector<T1> dataContainer;
+
+public:
+    auto push(const T1 &data)
+    {
+        this->dataContainer.push_back(data);
+    }
+
+    template <typename T2>
+    auto fmap(const std::function<T2(T1)> &func)
+    {
+        Functor<T2> functor;
+        for (const T1 &data : this->dataContainer)
+        {
+            functor.push(func(data));
+        }
+        return functor;
+    }
+
+    void printContainer() const
+    {
+        for (auto &data : this->dataContainer)
+        {
+            std::cout << data << ", ";
+        }
+        std::cout << std::endl;
+    }
+};
+
+// Example Functor Test
+void testFunctor(const std::vector<int> list)
+{
+    Functor<int> myFunctor;
+    for (const int &data : list)
+    {
+        myFunctor.push(data);
+    }
+    myFunctor.fmap<std::string>((std::function<std::string(int)>)[](const int &a) { return std::to_string(a); }).printContainer();
+}
+
+void test()
+{
+    testFunctor({1, 2, 3, 4, 5});
+}
--- test/testDriver.cpp
+++ test/testDriver.cpp
@@ -2,7 +2,8 @@
  * Run all tests from src library here.
  */
 
-#include "../src/Maybe.hpp"
+// #include "../src/Maybe.hpp"
+#include "../src/Functor.hpp"
 
 int main()
 {
