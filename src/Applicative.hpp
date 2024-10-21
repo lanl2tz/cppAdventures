0a274ce8b448374e6683746b94722763e3fbe574
lanl2tz <lanl2tz@hotmail.com>
2024-10-21 13:37:40 +0800

    applicative

---
+++ src/Applicative.hpp
@@ -0,0 +1,12 @@
+/**
+ * Implementation of Haskell applicative in modern C++.
+ */
+
+#include "Functor.hpp"
+
+template <typename T1>
+class Applicative : public Functor
+{
+public:
+    Applicative();
+};
